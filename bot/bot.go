f8e82666804726e359252fb011d1df30e4a8f434
James Harcourt <jh3g20@soton.ac.uk>
2021-12-04 16:42:40 +0000

    Made sneezy better

--- bot/bot.go
+++ bot/bot.go
@@ -3,7 +3,10 @@ package bot
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
 	"sneezy-bot/config"
+	"syscall"
 )
 
 var ownId string
@@ -35,6 +38,18 @@ func Start() {
 		fmt.Println(err.Error())
 		return
 	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	err = goBot.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -45,6 +60,11 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Content == config.BotPrefix+"bless" {
 		_, err := s.ChannelMessageSend(m.ChannelID, "Thank you.")
+		if err != nil {
+			return
+		}
+	} else if m.Content == "achoo" {
+		_, err := s.ChannelMessageSend(m.ChannelID, "Bless you!")
 		if err != nil {
 			return
 		}
